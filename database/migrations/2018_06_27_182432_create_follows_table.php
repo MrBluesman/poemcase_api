6e617a3a159200a3d5a195cd2dd74dd7e2a9c3f0
Łukasz <l.parysek@gmail.com>
2018-06-27 20:51:57 +0200

    Database structure - migrations.

---
+++ database/migrations/2018_06_27_182432_create_follows_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFollowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->integer('follower_id')->unsigned();
+            $table->integer('followed_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('follower_id')->references('id')->on('users');
+            $table->foreign('followed_id')->references('id')->on('users');
+            $table->primary(['follower_id', 'followed_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follows');
+    }
+}
--- database/migrations/2018_06_27_183604_create_publications_table.php
+++ database/migrations/2018_06_27_183604_create_publications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePublicationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('publications', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('first_name', 40)->nullable();
-            $table->string('last_name', 60)->nullable();
-            $table->string('description')->nullable();
+            $table->text('content');
             $table->boolean('is_private')->default(false);
-            $table->rememberToken();
+            $table->string('description')->nullable();
+            $table->boolean('is_comments_allowed')->default(true);
             $table->timestamps();
         });
     }
@@ -34,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('publications');
     }
 }
